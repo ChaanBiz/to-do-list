954842644a1e40e61b9b8157f1583bab7fd0fc4e
Chaan <rnchagwe27@gmail.com>
2024-05-28 21:59:07 +0800

    css alignment + js functions

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-let items = []
+let items = ["got"]
+
+const itemDiv = document.getElementById("items")
 
 function load() {
 
 }
 
 function render() {
+    itemDiv.innerHTML = null
 
+
+
+    for (const [idx, item] of Object.entries(items)) {
+        const container = document.createElement("div")
+        container.style.marginBottom = "10px"
+
+        const text = document.createElement("p")
+        text.textContent = item
+
+        const button = document.createElement("button")
+        button.textContent = "x"
+        button.onclick = () => remove(idx)
+
+        container.appendChild(text)
+        container.appendChild(button)
+
+
+        itemDiv.appendChild(container)
+    }
 }
+
+render()
 
 function save() {
 
@@ -16,6 +40,6 @@ function add() {
 
 }
 
-function remove() {
+function remove(idx) {
 
 }
