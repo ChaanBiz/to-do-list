2f8203104f6cf5a15cf820add614edb164415c15
Chaan <rnchagwe27@gmail.com>
2024-05-28 22:05:14 +0800

    functions added

--- main.js
+++ main.js
@@ -16,10 +16,19 @@ function render() {
         container.style.marginBottom = "10px"
 
         const text = document.createElement("p")
+        text.style.display = "inline"
+        text.style.marginRight = "10px"
+        text.style.fontSize = "18px"
         text.textContent = item
 
         const button = document.createElement("button")
-        button.textContent = "x"
+        button.textContent = "Delete"
+        button.style.padding = "5px 20px"
+        button.style.border = "none"
+        button.style.borderRadius = "20px"
+        button.style.backgroundColor = "rgb(172, 153, 129)"
+        button.style.color = "rgb(252, 246, 237)"
+        button.style.fontSize = "18px"
         button.onclick = () => remove(idx)
 
         container.appendChild(text)
